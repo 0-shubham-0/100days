6800c102012a3440c60f2ee8bf2106a6da4a99e6
Shubham Talawadekar <shubhamtalawadekar2002@gmail.com>
2021-09-17 11:31:06 +0530

    use Array

--- src/array.java
+++ src/array.java
@@ -8,8 +8,21 @@ public class array {
         number[1]=3;
         System.out.println("address is" + number);
         System.out.println("array is " + Arrays.toString(number));
-        int[] num={1,2,3,4,5};
+        int[] num={1,2,3,6,7,1,5};
         System.out.println(num.length);
+        System.out.println(Arrays.toString(num));
+        Arrays.sort(num);
+        System.out.println(Arrays.toString(num));
+
+        //matrix
+
+        int[][] num1 = new int[2][3];
+        num1[0][0] = 1;
+
+        // for matrix to string doesn't work
+        System.out.println(Arrays.deepToString(num1));
+        int[][] num2 = {{1,2,3,},{2,3,4}};
+        System.out.println(Arrays.deepToString(num2));
 
     }
 }
